2b337cb17b34e63ece594df7357ca0e16697cb53
Neely Nabors <nn127131@student.wpsok.org>
2026-08-26 13:01:05 -0500

    submitted HW2

--- HW1.py
+++ HW1.py
@@ -1,5 +1,15 @@
 #Name: Neely Nabors
 #Class: 5th Hour
-#Assignment: HW1
+#Assignment: HW2
+
+#1. Print Hello World!
+
+#2. Take the variables below and change the name of the variables to match the type of
+#variable they are: string, integer, or boolean
+
+#For example, the first one, Var1 = 4, should be:
+#integerVar1 = 4 or intVar1 = 4
+
+#DO NOT CHANGE THE VALUE OF THE VARIABLE.
 
 print("Hello World")
