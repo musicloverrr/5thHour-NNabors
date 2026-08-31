4fa11e5d02c321421ae979477901724b3e2d3c14
Neely Nabors <nn127131@student.wpsok.org>
2026-08-31 13:09:04 -0500

    submitted HW4

---
+++ HW4.py
@@ -0,0 +1,39 @@
+#Name: Neely Nabors
+#Class: 5th Hour
+#Assignment: HW4
+
+#1. Print "Hello World!"
+print("Hello World")
+
+#2. import the 'math' library
+import math
+
+#3. Create two variables, x and y, that asks the user for a decimal (float) for x and an integer for y.
+x = float(input("Give me a decimal: "))
+y = int(input("Give me an integer: "))
+
+#4. Create a variable with the value that is x and y added together.
+int_sum = x + y
+
+#5. Print the variable from #4.
+print(int_sum)
+
+#6. Create a variable with the value that is x and y added together, then divide the sum by 3.
+divide_by_3 = (x + y) / 3
+
+#7. Print the variable from #6.
+print(divide_by_3)
+
+#8. Create a variable with the value of the square root of y, then print the result.
+root = (math.sqrt(y))
+print(root)
+
+#9. Use the round function to round x to the nearest tenths place (EX: 1.17 rounds to 1.1). Print the result.
+print(round(x, 1))
+
+#10. Use the ceiling function to round x up to the nearest whole number. Print the result.
+print(math.ceil(x))
+
+#11. Use the floor function to round x down to the nearest whole number. Print the result.
+print(math.floor(x))
+
